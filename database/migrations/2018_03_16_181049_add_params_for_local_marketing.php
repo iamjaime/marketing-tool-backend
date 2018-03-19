fbf300263a63c7658e7c8244b320ce5f4b39aef8
CodeHead <iamjaime89@gmail.com>
2018-03-19 12:27:39 -0400

    added migration for local jobs

--- database/migrations/2018_03_16_181049_add_params_for_local_marketing.php
+++ database/migrations/2018_03_16_181049_add_params_for_local_marketing.php
@@ -20,6 +20,11 @@ class AddParamsForLocalMarketing extends Migration
             $table->string('locale')->after('dob')->nullable();
             $table->string('gender')->after('locale')->nullable();
         });
+
+        Schema::table('orders', function (Blueprint $table) {
+            $table->decimal('latitude', 10, 8)->after('url')->nullable();
+            $table->decimal('longitude', 11, 8)->after('latitude')->nullable();
+        });
     }
 
     /**
@@ -35,6 +40,11 @@ class AddParamsForLocalMarketing extends Migration
             $table->dropColumn('dob');
             $table->dropColumn('locale');
             $table->dropColumn('gender');
+        });
+
+        Schema::table('orders', function($table) {
+            $table->dropColumn('latitude');
+            $table->dropColumn('longitude');
         });
     }
 }
