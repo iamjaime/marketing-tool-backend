203a014b89db1da6565210c15e2df42c60151df0
CodeHead <iamjaime89@gmail.com>
2018-04-10 11:34:39 -0400

    added column for interests

--- database/migrations/2018_04_10_151152_add_data_to_users_table.php
+++ database/migrations/2018_04_10_151152_add_data_to_users_table.php
@@ -14,8 +14,13 @@ class AddDataToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+
+            $table->boolean('interested_in_working_with_smi')->after('interested_in')->default(false);
+            $table->boolean('interested_in_investing_in_smi')->after('interested_in_working_with_smi')->default(false);
+            $table->boolean('interested_in_using_smi_for_publicity')->after('interested_in_investing_in_smi')->default(false);
+
             //Working with SMI questions
-            $table->integer('earning_goal_amount')->after('interested_in')->nullable();
+            $table->integer('earning_goal_amount')->after('interested_in_using_smi_for_publicity')->nullable();
             $table->string('earning_currency')->after('earning_goal_amount')->nullable();
             $table->string('earning_frequency')->after('earning_currency')->nullable();
             $table->integer('daily_working_frequency')->after('earning_frequency')->nullable();
@@ -39,6 +44,11 @@ class AddDataToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function($table) {
+
+            $table->dropColumn('interested_in_working_with_smi');
+            $table->dropColumn('interested_in_investing_in_smi');
+            $table->dropColumn('interested_in_using_smi_for_publicity');
+
             $table->dropColumn('earning_goal_amount');
             $table->dropColumn('earning_currency');
             $table->dropColumn('earning_frequency');
